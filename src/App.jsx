ec7467367b5016de3443de3de2aac166868c6e38
Iman sarfi <sarfi831@gmail.com>
2024-08-30 09:30:43 -0700

    change for test

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
     <main>
       <h1>workOut Timer</h1>
-      <time>FOR YOUR WORKOUT ON %%SEPTEMBER 22 12:21 pm%%</time>
+      <time>FOR YOUR WORKOUT ON %%%SEPTEMBER 22 12:21 pm%%</time>
       <Calculator />
       <ToggleSound/>
     </main>
