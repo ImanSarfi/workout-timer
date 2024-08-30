cddb58dca11b989026549913d22f23c5ac0188fa
Iman sarfi <sarfi831@gmail.com>
2024-08-30 10:28:55 -0700

    set Date

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,24 @@ import ToggleSound from './ToggleSound';
 
 
 function App() {
-  const[allowSound, setAllowSound] = useState("false");
+  const[allowSound, setAllowSound] = useState(true);
+  const[time , setTime] = useState(formatTime(new Date()));
+
+  function formatTime(date) {
+    return new Intl.DateTimeFormat("en" ,{
+      month: "short",
+      year: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit"
+    }).format(date);
+  }
 
   return (
     <>
     <main>
       <h1>workOut Timer</h1>
-      <time>FOR YOUR WORKOUT ON %%%SEPTEMBER 22 12:21 pm%%</time>
+      <time>FOR YOUR WORKOUT ON {time}</time>
       <Calculator />
       <ToggleSound allowSound={allowSound} setAllowSound={setAllowSound}/>
     </main>
